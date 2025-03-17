6bf1da15ea24097c17e754a69b72c688e1806e57
uupb <93621948+uupb@users.noreply.github.com>
2025-03-16 19:08:11 -0700

    Add files via upload
    fixed to port not other, begin work on expanded boxed

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function navigateTo(page) {
-    window.location.href = page;
+// script.js
+function toggleExpand(element) {
+    // Toggle the 'expanded' class on the clicked element
+    element.classList.toggle('expanded');
+
+    // Collapse other expanded boxes
+    const allBoxes = document.querySelectorAll('.link-box.expandable');
+    allBoxes.forEach(box => {
+        if (box !== element && box.classList.contains('expanded')) {
+            box.classList.remove('expanded');
+        }
+    });
 }
